2814887d6c8432f70e2551b4faabdf95a526ae0d
Arildo65 <162459075+Arildo65@users.noreply.github.com>
2024-06-21 14:42:15 -0300

    Add files via upload

---
+++ SaudebemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.gov.fatec.saudebem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudebemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
